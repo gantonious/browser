1ec745fa7f4af499120e41a9ae3860ec2707efe6
George Antonious <george@antonious.ca>
2021-03-06 17:45:18 -0500

    [CssParser] Support parsing some named colors

--- src/main/kotlin/ca/antonious/browser/libraries/graphics/core/Color.kt
+++ src/main/kotlin/ca/antonious/browser/libraries/graphics/core/Color.kt
@@ -4,16 +4,27 @@ data class Color(val r: Float, val g: Float, val b: Float, val a: Float) {
     companion object {
         val black = Color(0f, 0f, 0f, 1f)
         val white = Color(1f, 1f, 1f, 1f)
-        val blue = Color(0f, 0f, 1f, 1f)
         val clear = Color(0f, 0f, 0f, 0f)
+        val red = Color(1f, 0f, 0f, 1f)
+        val green = Color(0f, 1f, 0f, 1f)
+        val blue = Color(0f, 0f, 1f, 1f)
+
     }
 }
 
 fun String.toColor(): Color {
-    val hexCode = trim().replace("#", "").toIntOrNull(16) ?: return Color.clear
-    val r = (hexCode and 0xFF0000) shr  16
-    val g = (hexCode and 0x00FF00) shr 8
-    val b = hexCode and 0x0000FF
+    return when (this.trim()) {
+        "white" -> Color.white
+        "red" -> Color.red
+        "blue" -> Color.blue
+        "green" -> Color.green
+        else -> {
+            val hexCode = trim().replace("#", "").toIntOrNull(16) ?: return Color.clear
+            val r = (hexCode and 0xFF0000) shr  16
+            val g = (hexCode and 0x00FF00) shr 8
+            val b = hexCode and 0x0000FF
 
-    return Color(r.toFloat() / 255f, g.toFloat() /255f, b.toFloat() / 255f, 1f)
+            Color(r.toFloat() / 255f, g.toFloat() /255f, b.toFloat() / 255f, 1f)
+        }
+    }
 }
