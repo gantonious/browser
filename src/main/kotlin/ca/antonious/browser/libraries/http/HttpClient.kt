aceb16b10a7dd9b1ae74f60ef1159b6cc771e131
George Antonious <george@antonious.ca>
2021-03-05 22:11:32 -0500

    [HttpClient] Resolve responses on the same thread

--- src/main/kotlin/ca/antonious/browser/libraries/http/HttpClient.kt
+++ src/main/kotlin/ca/antonious/browser/libraries/http/HttpClient.kt
@@ -3,10 +3,13 @@ package ca.antonious.browser.libraries.http
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.net.Socket
+import java.util.concurrent.Executors
 import javax.net.ssl.SSLSocket
 import javax.net.ssl.SSLSocketFactory
 
 class HttpClient {
+    private val mainThreadExecutor = Executors.newSingleThreadExecutor()
+
     fun execute(httpRequest: HttpRequest): Task<HttpResponse> {
         val requestUri = httpRequest.url
 
@@ -28,7 +31,10 @@ class HttpClient {
                 }
 
                 InputStreamReader(socket.getInputStream()).let {
-                    task.invokeSuccess(HttpResponseParser().parse(it.readText()))
+                    val response = HttpResponseParser().parse(it.readText())
+                    mainThreadExecutor.execute {
+                        task.invokeSuccess(response)
+                    }
                 }
             }
         }.start()
