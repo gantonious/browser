a512d03158e8f487cc61e9126148f852181e5bfa
George Antonious <george@antonious.ca>
2021-03-09 19:16:23 -0500

    [JavascriptConsole] Print string values wrapped in quotes

--- src/main/kotlin/ca/antonious/browser/libraries/javascript/main.kt
+++ src/main/kotlin/ca/antonious/browser/libraries/javascript/main.kt
@@ -1,5 +1,6 @@
 package ca.antonious.browser.libraries.javascript
 
+import ca.antonious.browser.libraries.javascript.ast.JavascriptValue
 import ca.antonious.browser.libraries.javascript.interpreter.JavascriptInterpreter
 import ca.antonious.browser.libraries.javascript.lexer.JavascriptLexer
 import ca.antonious.browser.libraries.javascript.parser.JavascriptParser
@@ -18,7 +19,12 @@ fun main() {
         try {
             val tokens = JavascriptLexer(lineToExecute).lex()
             val program = JavascriptParser(tokens, lineToExecute).parse()
-            println(interpreter.interpret(program))
+            println(
+                when (val value = interpreter.interpret(program)) {
+                    is JavascriptValue.String -> "'$value'"
+                    else -> value.toString()
+                }
+            )
         } catch (e: Exception) {
             println(e.message)
         }
