7676b2cad661aa81043746cb4d7bde05585e3da5
George Antonious <george@antonious.ca>
2021-03-10 22:19:23 -0500

    [Distribution] Use shadowjar to easily generate one executable jar for the browser

--- src/main/kotlin/ca/antonious/browser/Main.kt
+++ src/main/kotlin/ca/antonious/browser/Main.kt
@@ -4,6 +4,6 @@ import ca.antonious.browser.libraries.layout.libgdx.LibgdxLayoutRunner
 import ca.antonious.browser.libraries.web.WebContentLayout
 
 fun main() {
-    val webContentLayout = WebContentLayout(url = "http://skrundz.ca/15/")
+    val webContentLayout = WebContentLayout(url = "https://skrundz.ca/15/")
     LibgdxLayoutRunner().runLayout(webContentLayout)
 }
