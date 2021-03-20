9e6e25af87302f16203a46d81daee13835349968
George Antonious <george@antonious.ca>
2021-03-20 15:23:24 -0400

    [Browser] Accept initial url as argument to browser main

--- src/main/kotlin/ca/antonious/browser/Main.kt
+++ src/main/kotlin/ca/antonious/browser/Main.kt
@@ -3,7 +3,8 @@ package ca.antonious.browser
 import ca.antonious.browser.libraries.layout.libgdx.LibgdxLayoutRunner
 import ca.antonious.browser.libraries.web.WebContentLayout
 
-fun main() {
-    val webContentLayout = WebContentLayout(url = "https://skrundz.ca/15/")
+fun main(args: Array<String>) {
+    val url = args.firstOrNull() ?: error("Expected url to be passed as arg.")
+    val webContentLayout = WebContentLayout(url = url)
     LibgdxLayoutRunner().runLayout(webContentLayout)
 }
