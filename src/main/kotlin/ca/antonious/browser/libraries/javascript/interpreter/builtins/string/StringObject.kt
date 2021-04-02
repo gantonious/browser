e7e3e92a6a1d2e0dea5e90736b1e51f7452ada95
George Antonious <george@antonious.ca>
2021-04-02 14:50:50 -0400

    [JavaScriptEngine] Add length as property to string object

--- src/main/kotlin/ca/antonious/browser/libraries/javascript/interpreter/builtins/string/StringObject.kt
+++ src/main/kotlin/ca/antonious/browser/libraries/javascript/interpreter/builtins/string/StringObject.kt
@@ -1,8 +1,16 @@
 package ca.antonious.browser.libraries.javascript.interpreter.builtins.string
 
+import ca.antonious.browser.libraries.javascript.ast.JavascriptValue
 import ca.antonious.browser.libraries.javascript.interpreter.JavascriptObject
 
 class StringObject(val value: String) : JavascriptObject(StringPrototype) {
+
+    override fun getProperty(key: String): JavascriptValue {
+        return when (key) {
+            "length" -> JavascriptValue.Number(value.length.toDouble())
+            else -> super.getProperty(key)
+        }
+    }
     override fun toString(): String {
         return value
     }
