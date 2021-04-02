97887afee1adff701a5b471142ae7649d021c1e1
George Antonious <george@antonious.ca>
2021-04-02 14:54:29 -0400

    [JavaScriptEngine] Add source property to RegExp object

--- src/main/kotlin/ca/antonious/browser/libraries/javascript/interpreter/builtins/regex/RegExpObject.kt
+++ src/main/kotlin/ca/antonious/browser/libraries/javascript/interpreter/builtins/regex/RegExpObject.kt
@@ -1,8 +1,17 @@
 package ca.antonious.browser.libraries.javascript.interpreter.builtins.regex
 
+import ca.antonious.browser.libraries.javascript.ast.JavascriptValue
 import ca.antonious.browser.libraries.javascript.interpreter.JavascriptObject
 
 class RegExpObject(val regex: String, val flags: String) : JavascriptObject(RegExpPrototype) {
+
+    override fun getProperty(key: String): JavascriptValue {
+        return when (key) {
+            "source" -> JavascriptValue.String(regex)
+            else -> super.getProperty(key)
+        }
+    }
+
     override fun toString(): String {
         return "/$regex/$flags"
     }
