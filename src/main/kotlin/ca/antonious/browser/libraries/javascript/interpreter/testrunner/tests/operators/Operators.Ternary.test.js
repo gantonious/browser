bbd89b56d72fe48a1510b2d7eb6258148d1797db
George Antonious <george@antonious.ca>
2021-04-13 23:16:38 -0400

    [JavaScriptEngine] Add tests for ternary operator

---
+++ src/main/kotlin/ca/antonious/browser/libraries/javascript/interpreter/testrunner/tests/operators/Operators.Ternary.test.js
@@ -0,0 +1,16 @@
+describe("Operators.Ternary", () => {
+  test("returns first expression if condition is truthy", () => {
+    expect(true ? 0 : 1).toBe(0);
+  });
+
+  test("returns second expression if condition is truthy", () => {
+    expect(false ? 0 : 1).toBe(1);
+  });
+
+  test("supports nested ternary expression", () => {
+    // prettier-ignore
+    expect(true ? false ? 0 : 1 : true ? 2 : 3).toBe(1);
+    // prettier-ignore
+    expect(false ? false ? 0 : 1 : true ? 2 : 3).toBe(2);
+  });
+});
